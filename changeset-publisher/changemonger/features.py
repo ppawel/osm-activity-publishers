f41c02378001525f06dfe6efbe830937791adf3a
Paweł Paprota <ppawel@fastmail.fm>
2012-10-11 22:39:23 +0200

    Added Changemonger (osmdbapi branch).

---
+++ changeset-publisher/changemonger/features.py
@@ -0,0 +1,312 @@
+##  Changemonger: An OpenStreetMap change analyzer
+##  Copyright (C) 2012 Serge Wroclawski
+##
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU Affero General Public License as
+##  published by the Free Software Foundation, either version 3 of the
+##  License, or (at your option) any later version.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU Affero General Public License for more details.
+##
+##  You should have received a copy of the GNU Affero General Public License
+##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Contains functions related to Changemonger features for a yaml backend"""
+
+import inflect
+import yaml
+import os.path
+import imp
+
+inflection = inflect.engine()
+
+class BaseFeature:
+    """The base feature class"""
+    def __init__(self, name):
+        "Init the object"
+        self.name = name
+        self.types = []
+        self.categories = []
+        self.named = True
+        self.id = unicode(id(self))
+        self._prominence = 0
+
+    @property
+    def prominence(self, ele):
+        score = 0
+        tags = ele['tags']
+        if len(ele['tags']) > 0:
+            score += 1
+        if ( tags.get('name') or tags.get('brand') or tags.get('operator') ):
+            score += 2
+        if tag.get('historical') or tags.get('wikipedia'):
+            score += 3
+
+        return score + self._prominence
+
+    def _typecheck(self, ele):
+        "Check that the element matches this feature's type"
+        if self.types:
+            if ele['type'] in self.types:
+                return True
+            else:
+                return False
+        else:
+            return True
+
+    def category(self, cat):
+        "Add a category to this feature"
+        self.categories.append(cat)
+    
+    def match(self, element):
+        "Generic function"
+        # Never use this directly
+        return True
+
+    @property
+    def plural(self):
+        "Returns the plural version of the feature's name"
+        return inflection.plural(self.name)
+
+    @property
+    def precision(self):
+        "Returns the precision of the object. This should be set"
+        return 0
+
+class SimpleFeature(BaseFeature):
+    def __init__(self, name):
+        "Init simple feature"
+        self.tags = []
+        BaseFeature.__init__(self, name)
+
+    def tag(self, tg):
+        "Add a tag to object's tags"
+        self.tags.append(tg)
+
+    def match(self, element):
+        "Matches for simple features uses tags"
+        if self._typecheck(element):
+            for tag in self.tags:
+                if not tag in element['_tags']:
+                    return False
+            else:
+                return True
+        else:
+            return False
+
+    @property
+    def precision(self):
+        "Simple features have a precision of 10 + # of tags by default"
+        return 10 + len(self.tags)
+
+class Category(BaseFeature):
+    "Feature categories"
+    def __init__(self, name):
+        "Init a category"
+        self.features = []
+        BaseFeature.__init__(self, name)
+
+    def register(self, feature):
+        "Register a feature to this category"
+        self.features.append(feature)
+    
+    def match(self, element):
+        "The category checks all features for matches"
+        for feature in self.features:
+            if feature.match(element):
+                return True
+        return False
+
+    @property
+    def precision(self):
+        "Categories are precision 3 by default"
+        return 3
+
+def compare_precision(a, b):
+    """Compare the precision of two features"""
+    return b.precision - a.precision
+
+class FeatureDB:
+    """This is the abstraction against using the features"""
+    def __init__(self, directory = 'features'):
+        """Initialize feature database, use the argument as the directory"""
+        self._simple = []
+        self._magic = []
+        # We almost never iterate through categories, but we do call
+        # them by name a lot
+        self._categories = {}
+        # The index contains unique IDs for features
+        self._index = {}
+
+        # Now load the actual features
+        if not os.path.isabs(directory):
+            directory = os.path.abspath(directory)
+        # We're going to just assume the directory exists for now
+        
+        if os.path.exists(os.path.join(directory, 'features.yaml')):
+            self._load_yaml_simple_features(
+                os.path.join(directory, 'simple.yaml'))
+        elif os.path.isdir(os.path.join(directory, 'simple')):
+            self._load_simple_directory(os.path.join(directory, 'simple'))
+        
+        if os.path.exists(os.path.join(directory, 'categories.yaml')):
+            self._load_yaml_categories(os.path.join(directory,
+                                                    'categories.yaml'))
+
+        if os.path.exists(os.path.join(directory, 'magic.py')):
+            self._load_magic_file(directory)
+    
+    @property
+    def all(self):
+        return self._simple + self._categories.values() + self._magic
+
+    def _load_magic_file(self, directory):
+        fp, pathname, description = imp.find_module('magic', [directory])
+        try:
+            module = imp.load_module('magic', fp, pathname, description)
+            features = module.magic()
+            for feature in features:
+                self._magic.append(feature)
+                self._index[feature.id] = feature
+        finally:
+            if fp:
+                fp.close()
+
+    def _load_simple_directory(self, dirname):
+        for subdir, dirs, files in os.walk(dirname):
+            for fname in files:
+                name, ext = os.path.splitext(fname)
+                if ext == '.yaml' and name[0] != '.':
+                    self._load_yaml_simple_features(
+                        os.path.join(dirname, fname))
+
+    def _get_or_make_category(self, name):
+        """Either retrieve a category or create one as necessary"""
+        category = self._categories.get(name)
+        if not category:
+            category = Category(name)
+            self._categories[name] = category
+            self._index[category.id] = category
+        return category
+
+    def _yaml_item_to_feature(self, item):
+        feature = SimpleFeature(item['name'])
+        # type
+        if item.has_key('types'):
+            if isinstance(item['types'], basestring):
+                feature.types = item['types'].split(',')
+            else:
+                feature.types = item['types']
+        # id (virtually unused)
+        if item.has_key('id'):
+            feature.id = unicode(item['id'])
+        # tags
+        if isinstance(item['tags'], basestring):
+            tags = item['tags'].split(',')
+        else:
+            tags = item['tags']
+        for tag in tags:
+            feature.tag(tag)
+        # plural
+        if item.has_key('plural'):
+            feature.plural = item['plural']
+        # precision
+        if item.has_key('precision'):
+            feature.precision = int(item['precision'])
+        # categories
+        if item.has_key('categories'):
+            if isinstance(item['categories'], basestring):
+                categories = item['categories'].split(',')
+            else:
+                categories = item['categories']
+            for cat_name in categories:
+                category = self._get_or_make_category(cat_name)
+                category.register(feature)
+                feature.category(category)
+        # Named?
+        if item.has_key('named'):
+            feature.named = item['named']
+
+        # Prominence
+        if item.has_key('promience'):
+            feature._promience = item['prominence']
+
+        return feature
+
+    def _load_yaml_categories(self, fname):
+        """Load a yaml file full of categories into the database"""
+        with open(fname) as fd:
+            data = fd.read()
+            yamldata = yaml.safe_load(data)
+            for item in yamldata:
+                category = self._get_or_make_category(item['name'])
+                
+    def _load_yaml_simple_features(self, fname):
+        """Load a yaml of features file into the database"""
+        with open(fname) as fd:
+            data = fd.read()
+            yamldata = yaml.safe_load(data)
+            for item in yamldata:
+                # Make this a feature
+                feature = self._yaml_item_to_feature(item)
+                self._simple.append(feature)
+                self._index[feature.id] = feature
+    
+    def add_index(self, feature):
+        if self.get(feature.id):
+            print "BAD BAD BAD!!!! ID CONFLICT BETWEEN %s and %s" % (self.get(feature.id).name, feature.name)
+        self._index[feature.id] = feature
+
+    def matchBestSolo(self, ele):
+        """Returns the best matching feature for an element"""
+        # This function is not optimized in any way. Ideally it should
+        # run in mongodb or maybe even some other way, but it's quick
+        # and dirty and it works.
+        match = None
+        match_val = -10
+        for feature in self.all:
+            if feature.precision > match_val and feature.match(ele):
+                match = feature
+                match_val = feature.precision
+        return match
+
+    def matchAllSolo(self, ele):
+        """Return all the matching features and categories for an
+        element, sorted by precision
+        """
+        features = []
+        for feature in self.features:
+            if feature.match(ele):
+                features.append(feature)
+        return(sorted(features, cmp=compare_precision))
+
+    def matchEach(self, coll):
+        """Returns all the matches for all the elements in the collection"""
+        return [self.matchAllSolo(ele) for ele in coll]
+
+    def get(self, id):
+        "Retrieve an object by index id"
+        return self._index[id]
+
+    @property
+    def simple(self):
+        "Retrieve the simple features"
+        return self._simple
+    
+    @property
+    def categories(self):
+        "Retrieve the categories"
+        return self._categories.values()
+    
+    @property
+    def magic(self):
+        "Retrieve the magic features"
+        return self._magic
+
+    @property
+    def features(self):
+        "Return all features"
+        return self._simple + self._categories.values() + self._magic
---
+++ changeset-publisher/changemonger/osmdbapi.py
@@ -0,0 +1,178 @@
+##  Changemonger: An OpenStreetMap change analyzer
+##  Copyright (C) 2012 Serge Wroclawski
+##
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU Affero General Public License as
+##  published by the Free Software Foundation, either version 3 of the
+##  License, or (at your option) any later version.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU Affero General Public License for more details.
+##
+##  You should have received a copy of the GNU Affero General Public License
+##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Provides a simple abstraction against the OSM API"""
+"""Operations are implemented using PostGIS snapshot schema database instead of public API calls"""
+
+import psycopg2
+import psycopg2.extensions
+import psycopg2.extras
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+# Magic directory that contains OsmChange files named <changeset_id>.osc - one changeset per file.
+CHANGESETS_DIR = '/home/ppawel/src/changesets/'
+
+try:
+    dbconn = psycopg2.connect("dbname='osmdb' user='ppawel' host='localhost' password='aa'")
+except:
+    raise Exception("Unable to connect to the database")
+
+dbcursor = dbconn.cursor(cursor_factory = psycopg2.extras.DictCursor)
+# This will enable automatic hstore column -> dict conversion which is nice for columns with tags.
+psycopg2.extras.register_hstore(dbcursor, unicode = True)
+# Fix some unicode madness...
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
+
+def getNode(id, version = None):
+    id = str(id)
+    logging.debug("Retrieving node %s version %s" % (id, version))
+    dbcursor.execute("SELECT * FROM nodes WHERE id = %s" % id)
+    row = dbcursor.fetchone()
+    return createNodeXml(row)
+
+def getWay(id, version = None):
+    id = str(id)
+    logging.debug("Retrieving way %s version %s" % (id, version))
+    dbcursor.execute("SELECT * FROM ways w INNER JOIN way_nodes wn ON (wn.way_id = w.id) WHERE w.id = %s" % id)
+    rows = dbcursor.fetchall()
+    return createWayXml(rows)
+
+def getRelation(id, version = None):
+    id = str(id)
+    logging.debug("Retrieving relation %s version %s" % (id, version))
+    dbcursor.execute("SELECT * FROM relations r INNER JOIN relation_members rm ON (rm.relation_id = r.id) WHERE r.id = %s" % id)
+    rows = dbcursor.fetchall()
+    return createRelationXml(rows)
+
+def getChangeset(id):
+    return createChangesetXml(id)
+
+def getChange(id):
+    return open(CHANGESETS_DIR + id + ".osc").read()
+
+def getWaysforNode(id):
+    id = str(id)
+    logging.debug("Retrieving node %s ways" % id)
+    dbcursor.execute("SELECT * FROM ways w INNER JOIN way_nodes wn ON (wn.way_id = w.id) WHERE wn.node_id = %s ORDER BY w.id" % id)
+    rows = dbcursor.fetchall()
+    return createWayXml(rows)
+
+def getRelationsforElement(type, id):
+    id = str(id)
+    logging.debug("Retrieving relations for %s %s" % (type, str(id)))
+    dbcursor.execute("SELECT * FROM relations r INNER JOIN relation_members rm ON (rm.relation_id = r.id) WHERE rm.member_id = %s ORDER BY rm.relation_id, rm.member_type, rm.member_id" % id)
+    rows = dbcursor.fetchall()
+    return createRelationsXml(rows)
+
+## Caution: XML handling! Not pretty!
+
+from xml.dom.minidom import getDOMImplementation
+
+domimpl = getDOMImplementation()
+
+def createOsmDocument():
+    return domimpl.createDocument(None, "osm", None)
+
+def createNodeXml(node_row):
+    newdoc = createOsmDocument()
+    node_el = newdoc.createElement('node')
+    node_el.setAttribute('id', str(node_row['id']))
+
+    for k, v in node_row['tags'].items():
+        tag_el = newdoc.createElement('tag')
+        tag_el.setAttribute('k', k)
+        tag_el.setAttribute('v', v)
+        node_el.appendChild(tag_el)
+
+    newdoc.documentElement.appendChild(node_el)
+    return newdoc.toprettyxml(encoding = 'utf-8')
+
+def createWayXml(way_rows):
+    newdoc = createOsmDocument()
+    appendWayXml(newdoc, newdoc.documentElement, way_rows)
+    return newdoc.toprettyxml(encoding = 'utf-8')
+
+def createNodeWaysXml(way_rows):
+    newdoc = createOsmDocument()
+    appendWayXml(newdoc, newdoc.documentElement, way_rows)
+    return newdoc.toprettyxml(encoding = 'utf-8')
+
+def createRelationsXml(relation_rows):
+    newdoc = createOsmDocument()
+
+    if len(relation_rows) == 0:
+      return newdoc.toprettyxml(encoding = 'utf-8')
+
+    id = relation_rows[0]['relation_id']
+
+    for row in relation_rows:
+      if id != row['relation_id']:
+        appendWayXml(newdoc, newdoc.documentElement, way_rows)
+
+    return newdoc.toprettyxml(encoding = 'utf-8')
+
+def createChangesetXml(id):
+    newdoc = createOsmDocument()
+    changeset_el = newdoc.createElement('changeset')
+    changeset_el.setAttribute('id', str(id))
+    changeset_el.setAttribute('uid', str(1234))
+    changeset_el.setAttribute('user', str('abc'))
+    newdoc.documentElement.appendChild(changeset_el)
+    return newdoc.toprettyxml(encoding = 'utf-8')
+
+def appendWayXml(doc, el, way_rows):
+    if len(way_rows) == 0:
+      return
+
+    way_el = doc.createElement('way')
+    way_el.setAttribute('id', str(way_rows[0]['id']))
+
+    for node in way_rows:
+        node_el = doc.createElement('nd')
+        node_el.setAttribute('ref', str(node['id']))
+        way_el.appendChild(node_el)
+
+    for k, v in way_rows[0]['tags'].items():
+        tag_el = doc.createElement('tag')
+        tag_el.setAttribute('k', k)
+        tag_el.setAttribute('v', v)
+        way_el.appendChild(tag_el)
+
+    el.appendChild(way_el)
+
+def appendRelationXml(doc, el, relation_rows):
+    if len(relation_rows) == 0:
+      return
+
+    way_el = doc.createElement('way')
+    way_el.setAttribute('id', str(way_rows[0]['id']))
+
+    for node in way_rows:
+        node_el = doc.createElement('nd')
+        node_el.setAttribute('ref', str(node['id']))
+        way_el.appendChild(node_el)
+
+    for k, v in way_rows[0]['tags'].items():
+        tag_el = doc.createElement('tag')
+        tag_el.setAttribute('k', k)
+        tag_el.setAttribute('v', v)
+        way_el.appendChild(tag_el)
+
+    el.appendChild(way_el)
